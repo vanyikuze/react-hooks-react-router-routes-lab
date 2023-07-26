2e78be70c7972bca44b8788414643d3845794539
Vany <vany.ikuze@student.moringaschool.com>
2023-07-26 13:00:29 +0300

    Completed assignment

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,7 +2,17 @@ import React from "react";
 import { directors } from "../data";
 
 function Directors() {
-  return <div>{/*{code here}*/}</div>;
+  const directorList = directors.map((director) => (
+    <div key={director.name}>
+      <h2>{director.name}</h2>
+      <ul>
+        {director.movies.map((movie) => (
+          <li key={movie}>{movie}</li>
+        ))}
+      </ul>
+    </div>
+  ));
+  return <div><h1>Directors Page</h1>{directorList}</div>;
 }
 
 export default Directors;
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,19 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  const movieList = movies.map((movie) => (
+    <div key={movie.title}>
+      <h2>{movie.title}</h2>
+      <p>Runtime: {movie.time} min.</p>
+      <ul>
+        {movie.genres.map((genre) => (
+          <li key={genre}>{genre}</li>
+        ))}
+      </ul>
+    </div>
+  ));
+  return <div><h1>Movies Page</h1>
+  {movieList}</div>;
 }
 
 export default Movies;
